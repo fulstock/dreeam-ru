a78ab33607fd6b2a0d6570a400a1c63bb83bbca2
fulstock <fulstocky@gmail.com>
2025-04-23 13:13:12 +0300

    brat2docred upgraded

--- scripts/brat2docred.py
+++ scripts/brat2docred.py
@@ -21,46 +21,42 @@ morph = pymorphy2.MorphAnalyzer()
 
 brat2mrc_parser = argparse.ArgumentParser(description = "Brat to docred formatter script.")
 brat2mrc_parser.add_argument('--brat_dataset_path', type = str, required = True, help = "Path to brat dataset (with train, dev, test dirs).")
+brat2mrc_parser.add_argument('--subsets', type = str, default = ["train", "dev", "test"], help = "Subsets to convert, by default equals ['train', 'dev', 'test']. Should be None if a brat_dataset_path is a subset.")
 brat2mrc_parser.add_argument('--ner_path', type = str, required = True, help = 'Path to ner tags file with format ["CLASS1", "CLASS2", ...].')
 brat2mrc_parser.add_argument('--rel_path', type = str, required = True, help = 'Path to relation tags file with format ["CLASS1", "CLASS2", ...].')
 brat2mrc_parser.add_argument('--docred_output_path', type = str, default = None, help = "Path, where formatted dataset would be stored. By default, same path as in --brat_dataset_path would be used.")
+brat2mrc_parser.add_argument('--split_length', type = int, default = 128, help = "Split length.")
+brat2mrc_parser.add_argument('--split_diff', type = int, default = 4, help = "Split difference (amount in sentences).")
 
 args = brat2mrc_parser.parse_args()
 
 brat_dataset_path = args.brat_dataset_path
-
+split_length = args.split_length
+split_diff = args.split_diff
 docred_output_path = args.docred_output_path
+
 if docred_output_path is None:
     docred_output_path = brat_dataset_path
-
 if not os.path.exists(docred_output_path):
     os.makedirs(docred_output_path)
 
 ner_tags_path = args.ner_path
-
 with open(ner_tags_path, "r") as ner_tags_file:
     ner_tags = json.loads(ner_tags_file.read())
-
 ner_tags = ["BLANK"] + sorted(ner_tags)
-
 print(ner_tags)
 
 rel_tags_path = args.rel_path
-
 with open(rel_tags_path, "r") as rel_tags_file:
     rel_tags = json.loads(rel_tags_file.read())
-
 rel_tags = ["Na"] + sorted(rel_tags)
-
 print(rel_tags)
 
 rel_info = {}
 rel_info_inv = {}
 rel2id = {}
-
 rel2id["Na"] = 0
-
-for rel_id, rel_tag in enumerate([r for r in rel_tags[1:] if r != "ALTERNATIVE_NAME"]):
+for rel_id, rel_tag in enumerate([r for r in rel_tags[1:] if r != "ALTERNATIVE_NAME" and r != "ABBREVIATION"]):
     rel_info["P" + str(rel_id + 1)] = rel_tag
     # rel2id["P" + str(rel_id + 1)] = rel_id
     rel2id[rel_tag] = rel_id + 1
@@ -77,21 +73,24 @@ ner2id = {tag : tid for tid, tag in enumerate(ner_tags)}
 with open(os.path.join(docred_output_path, "ner2id.json"), "w") as ner2id_file:
     json.dump(ner2id, ner2id_file, ensure_ascii = False)
 
-#sets = ["train", "dev", "test"]
-
-sets = ["train", "test"]
-
-#sets = [""]
-
+sets = args.subsets
+if sets is None:
+    sets = [""]
 for ds in sets:
-
-    print(ds + " set:")
 
     total_relations_count = 0
     total_entity_mentions_count = 0
     total_entity_vertex_count = 0
     total_tokenizer_errors_count = 0
-    total_relations_errors_count = 0
+    total_split_errors = 0
+
+    dataset_path = os.path.join(brat_dataset_path, ds)
+
+    if len(ds) > 0:
+        print("Converting " + ds + " set:")
+    else:
+        print("Converting.")
+        ds = "dataset"
 
     jsonpath = os.path.join(docred_output_path, ds + ".json")
     jsondir = os.path.dirname(jsonpath)
@@ -100,8 +99,6 @@ for ds in sets:
         os.makedirs(jsondir)
 
     jsonfile = open(jsonpath, "w", encoding='UTF-8') 
-
-    dataset_path = os.path.join(brat_dataset_path, ds)
 
     docs_data = []
 
@@ -121,10 +118,9 @@ for ds in sets:
                     annfile = open(dataset_path + '/' + f, "r", encoding='UTF-8')
                     txtfile = open(dataset_path + '/' + f[:-4] + ".txt", "r", encoding='UTF-8')
 
-                    doc_data = {}
-
-                    doc_data["title"] = f[:-4]
-
+                    global_doc_data = {}
+                    global_doc_data["title"] = f[:-4]
+                    doc_datas = []
 
                     txtdata = txtfile.read()
 
@@ -214,7 +210,70 @@ for ds in sets:
 
                     start_words, end_words = zip(*offset_mapping)
 
-                    doc_data["sents"] = tokenized_sentences
+                    global_doc_data["sents"] = tokenized_sentences
+
+                    total_length = sum([len(s) for s in tokenized_sentences])
+                    # print(len(tokenized_sentences))
+                    # print([len(s) for s in tokenized_sentences])
+
+                    # print(total_length)
+                    if total_length > 512:
+                        split = True
+                        first_idx = 0
+                        curr_len = 0
+                        spk = 0
+                        for s_idx, sent in enumerate(tokenized_sentences):
+                            curr_len += len(sent)
+                            if curr_len > split_length:
+                                last_idx = s_idx
+                                
+                                # print(first_idx, last_idx, len(tokenized_sentences[first_idx : last_idx]), [len(sti) for sti in tokenized_sentences[first_idx : last_idx]], sum([len(sti) for sti in tokenized_sentences[first_idx : last_idx]]))
+        
+                                doc_datas.append({
+                                    "title" : global_doc_data["title"] + "_part" + str(spk),
+                                    "part" : spk,
+                                    "sents" : tokenized_sentences[first_idx : last_idx],
+                                    "global_sents_ids" : list(range(first_idx, last_idx))
+                                })
+                                spk += 1
+
+                                first_idx = s_idx - split_diff
+                                # print(s_idx)
+                                prev_len = sum([len(prev_sent) for prev_sent in tokenized_sentences[first_idx:s_idx + 1]])
+                                while prev_len > split_length:
+                                    # print(first_idx, s_idx + 1, prev_len)
+                                    first_idx += 1
+                                    prev_len = sum([len(prev_sent) for prev_sent in tokenized_sentences[first_idx:s_idx + 1]])
+                                # print(first_idx)
+                                curr_len = prev_len
+
+                        if curr_len > 0:
+                            last_idx = len(tokenized_sentences)
+                                    
+                            # print(first_idx, last_idx, len(tokenized_sentences[first_idx : last_idx]), [len(sti) for sti in tokenized_sentences[first_idx : last_idx]], sum([len(sti) for sti in tokenized_sentences[first_idx : last_idx]]))
+
+                            doc_datas.append({
+                                "title" : global_doc_data["title"] + "_part" + str(spk),
+                                "part" : spk,
+                                "sents" : tokenized_sentences[first_idx : last_idx],
+                                "global_sents_ids" : list(range(first_idx, last_idx))
+                            })
+                            spk += 1
+
+                    else:
+                        split = False
+                        doc_datas = [{
+                            "title" : global_doc_data["title"] + "_part0",
+                            "part" : 0,
+                            "sents" : tokenized_sentences,
+                            "global_sents_ids" : list(range(0, len(tokenized_sentences)))
+                        }]
+
+                    # print([len(doc_data["sents"]) for doc_data in doc_datas])
+                    # print([sum([len(s) for s in doc_data["sents"]]) for doc_data in doc_datas])
+                    # print([[len(s) for s in doc_data["sents"]] for doc_data in doc_datas])
+
+                    # exit(1)
                     
                     for entity_mentions_id, entity_type, start_char, end_char in zip(entity_mentions_ids, entity_types, entity_start_chars, entity_end_chars):
 
@@ -245,11 +304,23 @@ for ds in sets:
                             total_tokenizer_errors_count += 1
                             continue
 
+                        local_sent_id = sent_id
+                        part = 0
+                        if split:
+                            for d_idx, d in enumerate(doc_datas):
+                                if sent_id in d["global_sents_ids"]:
+                                    sent_offset = d["global_sents_ids"][0]
+                                    local_sent_id = sent_id - sent_offset
+                                    part = d_idx
+                                    break
+
                         if entity_lemm_mention not in unique_entity_mentions:
                             unique_entity_mentions[entity_lemm_mention] = [{
                                 "name" : entity_mention,
                                 "pos" : [start_word_idx, end_word_idx],
-                                "sent_id" : sent_id,
+                                "sent_id" : local_sent_id,
+                                "global_sent_id" : sent_id,
+                                "part" : part,
                                 "type" : entity_type,
                                 "mention_id" : entity_mentions_id
                             }]
@@ -257,17 +328,16 @@ for ds in sets:
                             unique_entity_mentions[entity_lemm_mention].append({
                                 "name" : entity_mention,
                                 "pos" : [start_word_idx, end_word_idx],
-                                "sent_id" : sent_id,
+                                "sent_id" : local_sent_id,
+                                "global_sent_id" : sent_id,
+                                "part" : part,
                                 "type" : entity_type,
                                 "mention_id" : entity_mentions_id
                             })
 
-                    doc_data["vertexSet"] = list(unique_entity_mentions.values())
-                    doc_data["labels"] = []
-
                     for rel_type, head_mention_id, tail_mention_id in zip(relation_types, head_mentions_ids, tail_mentions_ids):
 
-                        if rel_type != "ALTERNATIVE_NAME":
+                        if rel_type != "ALTERNATIVE_NAME" and rel_type != "ABBREVIATION":
                             continue
 
                         head_id = -1
@@ -304,42 +374,71 @@ for ds in sets:
 
                     total_entity_vertex_count += len(unique_entity_mentions)
 
-                    for rel_type, head_mention_id, tail_mention_id in zip(relation_types, head_mentions_ids, tail_mentions_ids):
+                    for part, doc_data in enumerate(doc_datas):
+                        if "vertexSet" not in doc_datas[part]:
+                            doc_datas[part]["vertexSet"] = []
+                        for vertex in list(unique_entity_mentions.values()):
+                            parted_vertex = [m for m in vertex if m["part"] == part and m["global_sent_id"] in doc_data["global_sents_ids"]]
+                            # print(parted_vertex, len(parted_vertex))
+                            # print()
+                            if "vertexSet" not in doc_datas[part]:
+                                if len(parted_vertex) > 0:
+                                    doc_datas[part]["vertexSet"] = [parted_vertex]
+                                else:
+                                    doc_datas[part]["vertexSet"] = []
+                            else:
+                                if len(parted_vertex) > 0:
+                                    doc_datas[part]["vertexSet"].append(parted_vertex)
 
-                        if rel_type == "ALTERNATIVE_NAME":
-                            continue
+                    # print(doc_datas[5])
 
-                        evidences = set()
-                        head_id = -1
-                        tail_id = -1
-                        for entity_id, vertex in enumerate(unique_entity_mentions.values()):
-                            for mention in vertex:
-                                if mention["mention_id"] == head_mention_id:
-                                    head_id = entity_id
-                                    evidences.add(mention["sent_id"])
-                                if mention["mention_id"] == tail_mention_id:
-                                    tail_id = entity_id
-                                    evidences.add(mention["sent_id"])
-                        if head_id < 0 or tail_id < 0:
-                            total_relations_errors_count += 1
-                            continue
-                        total_relations_count += 1
-                        same_relations = [(l_idx, l) for l_idx, l in enumerate(doc_data["labels"]) if l["r"] == rel_type and l["h"] == head_id and l["t"] == tail_id]
-                        assert len(same_relations) <= 1
-                        if len(same_relations) > 0:
-                            l_idx = same_relations[0][0]
-                            ev = set(doc_data["labels"][l_idx]["evidence"])
-                            ev.update(evidences)
-                            doc_data["labels"][l_idx]["evidence"] = sorted(list(ev))
-                        else:
-                            doc_data["labels"].append({
-                                "r" : rel_type,
-                                "h" : head_id,
-                                "t" : tail_id,
-                                "evidence" : sorted(list(evidences))
-                            })
+                    # print([len(doc_datas[part]["vertexSet"]) for part, doc_data in enumerate(doc_datas)])
+                    # print([doc_datas[part]["vertexSet"] for part, doc_data in enumerate(doc_datas)])
 
-                    docs_data.append(doc_data)
+                    for part, doc_data in enumerate(doc_datas):
+
+                        curr_mention_ids = [m["mention_id"] for v in doc_datas[part]["vertexSet"] for m in v]
+                        doc_datas[part]["labels"] = []
+
+                        for rel_type, head_mention_id, tail_mention_id in zip(relation_types, head_mentions_ids, tail_mentions_ids):
+
+                            if rel_type == "ALTERNATIVE_NAME" or rel_type == "ABBREVIATION":
+                                continue
+
+                            if head_mention_id not in curr_mention_ids or tail_mention_id not in curr_mention_ids:
+                                continue
+
+                            evidences = set()
+                            head_id = -1
+                            tail_id = -1
+                            for entity_id, vertex in enumerate(doc_datas[part]["vertexSet"]):
+                                for mention in vertex:
+                                    if mention["mention_id"] == head_mention_id:
+                                        head_id = entity_id
+                                        evidences.add(mention["sent_id"])
+                                    if mention["mention_id"] == tail_mention_id:
+                                        tail_id = entity_id
+                                        evidences.add(mention["sent_id"])
+                            
+                            total_relations_count += 1
+                            same_relations = [(l_idx, l) for l_idx, l in enumerate(doc_datas[part]["labels"]) if l["r"] == rel_type and l["h"] == head_id and l["t"] == tail_id]
+                            assert len(same_relations) <= 1
+                            if len(same_relations) > 0:
+                                l_idx = same_relations[0][0]
+                                ev = set(doc_datas[part]["labels"][l_idx]["evidence"])
+                                ev.update(evidences)
+                                doc_datas[part]["labels"][l_idx]["evidence"] = sorted(list(ev))
+                            else:
+                                doc_datas[part]["labels"].append({
+                                    "r" : rel_type,
+                                    "h" : head_id,
+                                    "t" : tail_id,
+                                    "evidence" : sorted(list(evidences))
+                                })
+
+                    total_split_errors += len(tokenized_sentences) - len(set([d for doc_data in doc_datas for d in doc_data["global_sents_ids"]])) 
+
+                    docs_data.extend(doc_datas)
 
 
                 except FileNotFoundError:
@@ -352,7 +451,7 @@ for ds in sets:
     print("Total entities:", total_entity_vertex_count)
     print("Total relations:", total_relations_count)
     print("Total tokenizing errors:", total_tokenizer_errors_count)
-    print("Total relation errors:", total_relations_errors_count)
+    print("Total split errors:", total_split_errors)
 
     json.dump(docs_data, jsonfile, ensure_ascii = False, indent = 2)
     jsonfile.close()
